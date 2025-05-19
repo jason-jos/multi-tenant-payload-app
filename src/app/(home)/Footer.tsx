0c4c1a258634bf385093b71a4b45c0c7cbd3ab53
Jason <Jason@BlueRock.com>
2025-05-19 19:06:14 +0530

    03 Layout,Navbar,side-navbar, footer

---
+++ src/app/(home)/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='border-t flex justify-between p-6'>
+        <p className='flex items-center gap-2 '>
+            funroad
+        </p>
+    </footer>
+  )
+}
+
+export default Footer
